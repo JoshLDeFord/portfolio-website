cc9afd5ebf7d049827b0621c961461f475220736
Joshua L DeFord <deford5708@gmail.com>
2022-11-05 23:26:33 -0500

    deploy

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Under construction!</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
